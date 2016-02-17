2004cbe20e2e39ffee3842abc985752f63aed5bc
Scott Donaldson <scott.p.donaldson@gmail.com>
2016-02-16 21:17:17 -0500

    starting on modal

--- footer.php
+++ footer.php
@@ -2,6 +2,21 @@
 
 <footer class="return-to-top uppercase">Return to top &#8593;</footer>
 
+<div id="pop" class="bg-white-trans">
+	
+</div>
+
+<div id="modal" class="bg-white-trans">
+	<div class="modal-content bg-white">
+		<p>Want to receive <i>very</i> occasional brand updates and special discounts? Join the <b>DDUGOFF</b> mailing list.</p>
+		<p>We won't spam you. We'll just keep you in the loop.</p>
+	</div>
+	<form class="clearfix">
+		<input class="email" type="email" placeholder="Email" required>
+		<input class="submit" type="submit" value="Submit">
+	</form>
+</div>
+
 </div><!-- #page -->
 
 <script src="<?= bloginfo('template_url'); ?>/js/min/script.min.js"></script>
