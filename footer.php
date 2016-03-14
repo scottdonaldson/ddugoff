8bb1f743b86aafe78aff28a616ebe904541158da
Scottland Donaldson <scott.p.donaldson@gmail.com>
2016-03-14 16:32:35 -0400

    form doesn't open in new tab

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 		<a class="icon-twitter" href="https://twitter.com/ddugoff" target="_blank"></a>
 		<a class="icon-instagram" href="https://www.instagram.com/ddugoff" target="_blank"></a>
 
-		<form action="//ddugoff.us10.list-manage.com/subscribe/post?u=29ac020e64f4734de2809b235&amp;id=60fcba0dcb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+		<form action="//ddugoff.us10.list-manage.com/subscribe/post?u=29ac020e64f4734de2809b235&amp;id=60fcba0dcb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
 			<input class="email" type="email" name="EMAIL" id="mce-EMAIL" placeholder="Email" required>
 			<input type="hidden" name="b_29ac020e64f4734de2809b235_60fcba0dcb" tabindex="-1" value="">
 			<input class="submit" type="submit" name="subscribe" id="mc-embedded-subscribe" value="Submit">
