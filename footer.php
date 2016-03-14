e648398d8ea7e384d787193c3ad1e17fab1c20a0
Scottland Donaldson <scott.p.donaldson@gmail.com>
2016-03-14 16:35:04 -0400

    template, .email width

--- footer.php
+++ footer.php
@@ -23,9 +23,10 @@
 		<p>Want to receive <i>very</i> occasional brand updates and special discounts? Join the <b>DDUGOFF</b> mailing list.</p>
 		<p>We won't spam you. We'll just keep you in the loop.</p>
 	</div>
-	<form class="clearfix">
-		<input class="email" type="email" placeholder="Email" required>
-		<input class="submit" type="submit" value="Submit">
+	<form action="//ddugoff.us10.list-manage.com/subscribe/post?u=29ac020e64f4734de2809b235&amp;id=60fcba0dcb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
+		<input class="email" type="email" name="EMAIL" id="mce-EMAIL" placeholder="Email" required>
+		<input type="hidden" name="b_29ac020e64f4734de2809b235_60fcba0dcb" tabindex="-1" value="">
+		<input class="submit" type="submit" name="subscribe" id="mc-embedded-subscribe" value="Submit">
 	</form>
 </div>
 
---
+++ signup.php
@@ -0,0 +1,5 @@
+<form action="//ddugoff.us10.list-manage.com/subscribe/post?u=29ac020e64f4734de2809b235&amp;id=60fcba0dcb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
+	<input class="email" type="email" name="EMAIL" id="mce-EMAIL" placeholder="Email" required>
+	<input type="hidden" name="b_29ac020e64f4734de2809b235_60fcba0dcb" tabindex="-1" value="">
+	<input class="submit" type="submit" name="subscribe" id="mc-embedded-subscribe" value="Submit">
+</form>
