fcb3cc4674ea1669eb5aaaa8f1400f82a6c03c9c
Scott Donaldson <scott.p.donaldson@gmail.com>
2016-03-14 19:56:03 -0400

    actually use template, open link in new tab

--- footer.php
+++ footer.php
@@ -8,11 +8,7 @@
 		<a class="icon-twitter" href="https://twitter.com/ddugoff" target="_blank"></a>
 		<a class="icon-instagram" href="https://www.instagram.com/ddugoff" target="_blank"></a>
 
-		<form action="//ddugoff.us10.list-manage.com/subscribe/post?u=29ac020e64f4734de2809b235&amp;id=60fcba0dcb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
-			<input class="email" type="email" name="EMAIL" id="mce-EMAIL" placeholder="Email" required>
-			<input type="hidden" name="b_29ac020e64f4734de2809b235_60fcba0dcb" tabindex="-1" value="">
-			<input class="submit" type="submit" name="subscribe" id="mc-embedded-subscribe" value="Submit">
-		</form>
+		<?php get_template_part('signup'); ?>
 	</div>
 	
 	<span class="open">+</span>
@@ -23,11 +19,7 @@
 		<p>Want to receive <i>very</i> occasional brand updates and special discounts? Join the <b>DDUGOFF</b> mailing list.</p>
 		<p>We won't spam you. We'll just keep you in the loop.</p>
 	</div>
-	<form action="//ddugoff.us10.list-manage.com/subscribe/post?u=29ac020e64f4734de2809b235&amp;id=60fcba0dcb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
-		<input class="email" type="email" name="EMAIL" id="mce-EMAIL" placeholder="Email" required>
-		<input type="hidden" name="b_29ac020e64f4734de2809b235_60fcba0dcb" tabindex="-1" value="">
-		<input class="submit" type="submit" name="subscribe" id="mc-embedded-subscribe" value="Submit">
-	</form>
+	<?php get_template_part('signup'); ?>
 </div>
 
 </div><!-- #page -->
